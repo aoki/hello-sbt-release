c8f2d769b56436e8fe659f59784d7e5a3faed386
Yoshiki Aoki <ringohub@gmail.com>
2015-06-24 00:13:59 +0900

    Add publish setting

--- project/Build.scala
+++ project/Build.scala
@@ -4,12 +4,14 @@ import Keys._
 object ProjectBuild extends Build {
 
   val ScalaVersion = "2.11.6"
+  val PublishLocation = "./target/releases"
 
   lazy val root = Project(
     id = "sbt-release-sample",
     base = file("."),
     settings = Seq(
-      scalaVersion := ScalaVersion
+      scalaVersion := ScalaVersion,
+      publishTo := Some(Resolver.file("file",  new File( PublishLocation )) )
     )
   )
 
